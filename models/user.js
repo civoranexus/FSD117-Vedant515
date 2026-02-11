375d6f7e81cdc77d40d94c2b3851836779682631
Vedant515 <vedantpatil2402@gmail.com>
2026-02-11 20:30:04 +0530

    initial commit

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["admin", "vendor"], default: "vendor" },
+  vendorCode: String,
+  status: { type: String, default: "active" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
